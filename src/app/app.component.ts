65e44a3168b50bbb1a52927fe72915ded793d491
Damian Ruczynski <damian.m.ruczynski@gmail.com>
2025-02-21 22:52:53 +0100

    add ngx translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,26 @@ import { RouterOutlet } from '@angular/router';
 import { ProjectService } from './core/service/project.service';
 import { AsyncPipe } from '@angular/common';
 import { Category, Room, TProject } from './core/models/project.model';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   projects: TProject[] = [];
-  constructor(public projectService: ProjectService) {}
+
+  constructor(
+    public projectService: ProjectService,
+    private translate: TranslateService
+  ) {
+    this.translate.addLangs(['pl', 'en']);
+    this.translate.setDefaultLang('en');
+    this.translate.use('pl');
+  }
   async ngOnInit() {
     this.projects = await this.projectService.getProjects(
       Room.BATHROOM,
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { initializeApp } from 'firebase/app';
 import { routes } from './app.routes';
 import { environment } from '../environments/environment';
+
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (
+  http: HttpClient
+) => new TranslateHttpLoader(http, './i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,5 +25,15 @@ export const appConfig: ApplicationConfig = {
       provide: 'FirebaseApp',
       useFactory: () => initializeApp(environment.firebase),
     },
+    provideHttpClient(),
+    importProvidersFrom([
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: httpLoaderFactory,
+          deps: [HttpClient],
+        },
+      }),
+    ]),
   ],
 };
