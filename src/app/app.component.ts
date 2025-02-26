2dc74e2cfea00f64d36a86ee76a2da65bb93dc8f
Damian Ruczynski <damian.m.ruczynski@gmail.com>
2025-02-26 14:51:03 +0100

    siedbar navigation mobile rwd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject } from '@angular/core';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { MenuComponent } from './shared/menu/menu.component';
 import { MatDialog } from '@angular/material/dialog';
@@ -7,6 +7,11 @@ import { DialogComponent } from './ui/dialog/dialog.component';
 import { DialogData } from './ui/dialog/dialog.model';
 import { MatIconModule } from '@angular/material/icon';
 import { LanguageComponent } from './ui/language/language.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+import { MediaMatcher } from '@angular/cdk/layout';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -18,6 +23,9 @@ import { LanguageComponent } from './ui/language/language.component';
     MenuComponent,
     MatIconModule,
     LanguageComponent,
+    NgClass,
+    MatSidenavModule,
+    MatButtonModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
@@ -25,12 +33,20 @@ import { LanguageComponent } from './ui/language/language.component';
 // TODO create in node js admin for add new projects (to automatize process of adding new projects for client)
 export class AppComponent {
   readonly dialog = inject(MatDialog);
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
   constructor(private translate: TranslateService) {
     this.translate.addLangs(['pl', 'en']);
     this.translate.setDefaultLang('en');
     this.translate.use(this.translate.getBrowserLang() === 'pl' ? 'pl' : 'en');
-  }
 
+    const changeDetectorRef = inject(ChangeDetectorRef);
+    const media = inject(MediaMatcher);
+
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
   public openContactDialog(): void {
     const data: DialogData = {
       title: 'contact.title',
